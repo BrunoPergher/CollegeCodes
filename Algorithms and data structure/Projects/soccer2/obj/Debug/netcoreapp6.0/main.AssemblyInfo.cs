bc05599136ded93654f826879081fb13f3981193
Bruno Pergher <brunopergher_1@hotmail.com>
2024-09-30 00:48:12 -0300

    Implemented philosopher dinner solution
    philosopher dinner solution avoiding dead locks and starvation

--- Algorithms and data structure/Projects/soccer2/obj/Debug/netcoreapp6.0/main.AssemblyInfo.cs
+++ Algorithms and data structure/Projects/soccer2/obj/Debug/netcoreapp6.0/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20bba08294877ba9a7e3bb9e9f72a28383667707")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7340717f4979b87ef41f0bef6f5cb2dc89fd93c")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
