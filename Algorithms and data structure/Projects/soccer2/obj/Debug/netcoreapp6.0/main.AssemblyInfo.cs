a4a9a65bdf18b8bb6ea37f0775811379643ea43a
Bruno Pergher <brunopergher_1@hotmail.com>
2024-10-29 00:02:53 -0300

    Add codes of data structure college codes

--- Algorithms and data structure/Projects/soccer2/obj/Debug/netcoreapp6.0/main.AssemblyInfo.cs
+++ Algorithms and data structure/Projects/soccer2/obj/Debug/netcoreapp6.0/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1af84e5e45d656f62ce5dbf8e9a82924d65501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4601ddc006cb0b8ff65a0fab7baa91521df0c902")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
